6f2cc56465daba14d55e56178eac174a2cbad294
chenwei <981661072@qq.com>
2020-05-01 14:59:26 +0800

    .

--- react-demo/src/components/pages/home/index.jsx
+++ react-demo/src/components/pages/home/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-// import {Button } from 'antd'
+import {Button } from 'antd'
 
 export default class Home extends React.Component{
     render(){
         return(
             <>
-                {/* <Button type="primary">Primary</Button> */}
+                <Button type="primary">Primary</Button>
                 {/* <Breadcrumb>
                     <Breadcrumb.Item>Home</Breadcrumb.Item>
                     <Breadcrumb.Item>
