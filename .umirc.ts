f53f57fb08b5d4359a91c81f973d2fb4aa838ba8
chenwei <981661072@qq.com>
2021-01-23 13:52:40 +0800

    项目初始化

---
+++ .umirc.ts
@@ -0,0 +1,41 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' },
+        {
+          path:'/threeDemo/webgl_animation_cloth',
+          component:'../pages/three-demo/webgl_animation_cloth'
+        }
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'three-demo-react',
+      dll: false,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
+
+export default config;
---
+++ src/pages/three-demo/webgl_animation_cloth/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'dva'
+
+const webgl_animation_cloth: React.FC = (props:any) => {
+    return(
+        <div>webgl_animation_cloth</div>
+    )
+}
+
+export default connect()(webgl_animation_cloth)
